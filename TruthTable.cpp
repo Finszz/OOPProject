a36035d4b28d9e68da1c8b4dcd1b0fb58f8c293d
Finszz <akbara2706@gmail.com>
2026-04-16 17:54:27 +0100

    fixinggenandsave

--- TruthTable.cpp
+++ TruthTable.cpp
@@ -8,41 +8,28 @@ using namespace std;
 TruthTable::TruthTable(BooleanExpression logic) : expr(logic) {}
 
 void TruthTable::generate() {
-
     cout << "\nTruth Table:\n";
-    cout << "|A | B | C | result |\n";
-
+    cout << "|A|B|C|result|\n";
     for (int i = 0; i < 8; i++) {
-
-        bool A = (i & 4);
-        bool B = (i & 2);
-        bool C = (i & 1);
-
+        bool A = (i & 4) != 0;
+        bool B = (i & 2) != 0;
+        bool C = (i & 1) != 0;
         bool result = expr.evaluate(A, B, C);
-
-        cout << "| " << setw(1) << A << " | " << setw(1) << B << " | " << setw(1) << C << " | " << setw(6) << result << " |\n";
+        cout << "|" << A << "|" << B << "|" << C << "|" << result << "|\n";
     }
 }
 
 void TruthTable::save(string filename, string expression) {
-
     ofstream file(filename + ".txt");
-
     file << "Expression: " << expression << "\n\n";
-    file << "| A | B | C | result |\n";
-
+    file << "|A|B|C|result|\n";
     for (int i = 0; i < 8; i++) {
-
-        bool A = (i & 4);
-        bool B = (i & 2);
-        bool C = (i & 1);
-
+        bool A = (i & 4) != 0;
+        bool B = (i & 2) != 0;
+        bool C = (i & 1) != 0;
         bool result = expr.evaluate(A, B, C);
-
-        file << "| " << setw(1) << A << " | " << setw(1) << B << " | " << setw(1) << C << " | " << setw(6) << result << " |\n";
+        file << "|" << A << "|" << B << "|" << C << "|" << result << "|\n";
     }
-
     file.close();
-
     cout << "Saved.\n";
 }
