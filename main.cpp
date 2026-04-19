28a1a0e22af8d6749b29f6f8d1d9e7ef240a24bd
Finszz <akbara2706@gmail.com>
2026-04-19 01:12:30 +0100

    finalfinalmain

--- main.cpp
+++ main.cpp
@@ -12,6 +12,38 @@ int main() {
     cout << "Enter Boolean Expression:\n";
     getline(cin, input);
 
+    cout << "\nOperators Detected and Explained:\n";
+
+    if (input.find("AND") != string::npos)
+    {
+    cout << "- AND : True only if both inputs are true.\n";
+    }
+
+    if (input.find("OR") != string::npos)
+    {
+    cout << "- OR  : True if at least one input is true.\n";
+    }
+
+    if (input.find("NOT") != string::npos)
+    {
+    cout << "- NOT : Inverts the input.\n";
+    }
+
+    if (input.find("XOR") != string::npos)
+    {   
+    cout << "- XOR : True if both inputs are different.\n";
+    }
+
+    if (input.find("NAND") != string::npos)
+    {
+    cout << "- NAND: Opposite of AND.\n";
+    }
+
+    if (input.find("NOR") != string::npos)
+    {
+    cout << "- NOR : Opposite of OR.\n";
+    }
+
     BooleanExpression inputTT(input);
 
     TruthTable table(inputTT);
